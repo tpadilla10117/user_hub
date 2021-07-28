dfae41549365a72cabfb7543af1520598f6a7303
Trinidad Padilla <tpadilla101@live.com>
2021-07-28 12:19:06 -0700

    FINALLY GOT IT RENDERED, the .map upon initial render

--- src/components/user-card/user-card.jsx
+++ src/components/user-card/user-card.jsx
@@ -6,87 +6,25 @@ const UserCard = (props) => {
     
     
     const BASE_URL = "https://jsonplace-univclone.herokuapp.com";
-    const [ user, setUser ] = useState();
-    const [ stateHelper ] = useState('');
-
-    const dataArr = [
-
-        
-            {
-            id: 1,
-            name: "Leanne Graham",
-            username: "Bret",
-            email: "Sincere@april.biz",
-                address: {
-                street: "Kulas Light",
-                suite: "Apt. 556",
-                city: "Gwenborough",
-                zipcode: "92998-3874",
-                    geo: {
-                    lat: "-37.3159",
-                    lng: "81.1496"
-                    }
-                },
-            phone: "1-770-736-8031 x56442",
-            website: "hildegard.org",
-                company: {
-                name: "Romaguera-Crona",
-                catchPhrase: "Multi-layered client-server neural-net",
-                bs: "harness real-time e-markets"
-                }
-            },
-            {
-            id: 2,
-            name: "Ervin Howell",
-            username: "Antonette",
-            email: "Shanna@melissa.tv",
-            address: {
-            street: "Victor Plains",
-            suite: "Suite 879",
-            city: "Wisokyburgh",
-            zipcode: "90566-7771",
-            geo: {
-            lat: "-43.9509",
-            lng: "-34.4618"
-            }
-            },
-            phone: "010-692-6593 x09125",
-            website: "anastasia.net",
-            company: {
-            name: "Deckow-Crist",
-            catchPhrase: "Proactive didactic contingency",
-            bs: "synergize scalable supply-chains"
-            }
-            }
-    ]
-
-    /* const { user } = props; */
-    /* console.log("From the user component:", user)
-    console.log("Result of calling the User in component user-card:", user) */
+    const [ user, setUser ] = useState(null);
 
     useEffect( () => { 
+
         async function fetchUsers() {
-          try {
-            const response = await fetch(`${BASE_URL}/users`);
-            const json = await response.json();
-            console.log("Here is the result of the async function:", json);
-            console.log(user)
-            setUser(json)
-            console.log(user[1])
-            console.log(typeof user[3])
-            /* return json; */
-            /* return json; */ //can return array data if accessed here
-          } catch(error) {
-            console.error(error);
-          }
-        };
+            try {
+                
+                const response = await fetch(`${BASE_URL}/users`);
+                response.json().then( result => setUser(result));
+              
+            } catch(error) {
+              console.error(error);
+            }
+          };
+        
         fetchUsers();
-        console.log(user)
         
       }, []);
       
-
-    
     /* To template user-cards in the UI: */
     const renderUser = (user, index) => {
         return(
@@ -109,7 +47,7 @@ const UserCard = (props) => {
     
     return (
         <>
-        {dataArr.map(renderUser)}; {/* address and company are nested objects within objects, geo is another level down */}
+        {user && user.map(renderUser)}; {/* Needed ```user && user.map``` to work*/}
         </>
     )
 }
