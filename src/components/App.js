1af2b7ca3141fa0f90db6e04052a7e209332736c
Trinidad Padilla <tpadilla101@live.com>
2021-07-28 10:53:29 -0700

    fixed map problem, but need to fix state problem

--- src/components/App.js
+++ src/components/App.js
@@ -4,50 +4,12 @@ import { SectionWrapper, UserList, UserCard} from './index';
 
 function App() {
 
-  //Should I make API call for user data here?
-  const BASE_URL = "https://jsonplace-univclone.herokuapp.com";
-  const [ user, setUser ] = useState(fetchUsers() );
-  //This is running an infinite loop...
-  
-/*   function fetchUsers() {
-      fetch(`${BASE_URL}/users`).then( response => response.json() )
-      .then( result => setUser(result))
-      .catch(function (error) {
-          console.error(error);
-      })
-  }; */
-
-  async function fetchUsers() {
-    try {
-      const response = await fetch(`${BASE_URL}/users`);
-      const json = await response.json();
-      console.log("Here is the result of the async function:", json);
-      /* return setUser(json); */
-      return json; //can return array data if accessed here
-    } catch(error) {
-      console.error(error);
-    }
-  };
-
-  /* fetchUsers(); */
-
- /*  useEffect( () => {
-    fetchUsers();
-  }, []); */
-  
-  /* fetchUsers().then(function (data) {
-    console.log(data);
-    return data;
-  }); */
-
-console.log("Here is my user state:", user)
-
-
+ 
 
 
   return (
     <div>
-      <UserList content={ <UserCard user={user}/> }>
+      <UserList content={ <UserCard /> }>
       </UserList>
       <SectionWrapper id="instructions" className="active">
         
--- src/components/user-card/user-card.jsx
+++ src/components/user-card/user-card.jsx
@@ -1,22 +1,92 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 /* The user-card components, rendered with data from renderUser function */
 
 const UserCard = (props) => {
+    
+    
+    const BASE_URL = "https://jsonplace-univclone.herokuapp.com";
+    const [ user, setUser ] = useState();
+    const [ stateHelper ] = useState('');
 
-    const { user } = props;
-    console.log("From the user component:", user)
-    console.log("Result of calling the User in component user-card:", user)
+    const dataArr = [
 
+        
+            {
+            id: 1,
+            name: "Leanne Graham",
+            username: "Bret",
+            email: "Sincere@april.biz",
+                address: {
+                street: "Kulas Light",
+                suite: "Apt. 556",
+                city: "Gwenborough",
+                zipcode: "92998-3874",
+                    geo: {
+                    lat: "-37.3159",
+                    lng: "81.1496"
+                    }
+                },
+            phone: "1-770-736-8031 x56442",
+            website: "hildegard.org",
+                company: {
+                name: "Romaguera-Crona",
+                catchPhrase: "Multi-layered client-server neural-net",
+                bs: "harness real-time e-markets"
+                }
+            },
+            {
+            id: 2,
+            name: "Ervin Howell",
+            username: "Antonette",
+            email: "Shanna@melissa.tv",
+            address: {
+            street: "Victor Plains",
+            suite: "Suite 879",
+            city: "Wisokyburgh",
+            zipcode: "90566-7771",
+            geo: {
+            lat: "-43.9509",
+            lng: "-34.4618"
+            }
+            },
+            phone: "010-692-6593 x09125",
+            website: "anastasia.net",
+            company: {
+            name: "Deckow-Crist",
+            catchPhrase: "Proactive didactic contingency",
+            bs: "synergize scalable supply-chains"
+            }
+            }
+    ]
 
-    /* const tester = user.map(item => ({ name: item.name, email: item.email}))
-    console.log(tester); */
+    /* const { user } = props; */
+    /* console.log("From the user component:", user)
+    console.log("Result of calling the User in component user-card:", user) */
 
-    //Im getting back undefined when I want individual values
+    useEffect( () => { 
+        async function fetchUsers() {
+          try {
+            const response = await fetch(`${BASE_URL}/users`);
+            const json = await response.json();
+            console.log("Here is the result of the async function:", json);
+            console.log(user)
+            setUser(json)
+            console.log(user[1])
+            console.log(typeof user[3])
+            /* return json; */
+            /* return json; */ //can return array data if accessed here
+          } catch(error) {
+            console.error(error);
+          }
+        };
+        fetchUsers();
+        console.log(user)
+        
+      }, []);
+      
 
-    //comes back as an object, which is why no array methods work
-    /* console.log("Here is an example of user data", typeof user) */
-
+    
     /* To template user-cards in the UI: */
     const renderUser = (user, index) => {
         return(
@@ -26,8 +96,8 @@ const UserCard = (props) => {
             </header>
             <section className="company-info">
                 <p><b>Contact:</b>{user.email}</p>
-                <p><b>Works for:</b>{user.company}</p>
-                <p><b>Company creed:</b>{user.company}</p>
+                <p><b>Works for:</b>{user.company.name}</p>
+                <p><b>Company creed:</b>{user.company.catchPhrase}</p>
             </section>
             <footer>
                 <button className="load-posts">POSTS BY {user.username}</button>
@@ -39,7 +109,7 @@ const UserCard = (props) => {
     
     return (
         <>
-        {/* {user.map(renderUser) }; */}
+        {dataArr.map(renderUser)}; {/* address and company are nested objects within objects, geo is another level down */}
         </>
     )
 }
